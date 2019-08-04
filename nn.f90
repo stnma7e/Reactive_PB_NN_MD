450ea74e2a54781f258b9e2d81ac7091eb46463f
Sam Delmerico <svdelmerico@gmail.com>
2019-08-03 22:24:26 -0400

    all pieces implemented; not tested (probably doesnt work)

--- nn.f90
+++ nn.f90
@@ -1,33 +1,96 @@
 program nn
     implicit none
 
-    real,dimension(3,3) :: weights
-    real,dimension(3) :: a, bias
-    integer :: i, j
+    real,allocatable :: weights(:,:,:)
+    real,allocatable :: bias(:,:), zs(:,:), as(:,:), errors(:,:)
+    real,dimension(3) :: example
+    integer :: i, n_layers
 
-    bias = (/ 1.0, 2.0, 3.0 /)
-    call random_number(a)
+    n_layers = 10
+    allocate(weights(n_layers,3,3), bias(n_layers,3), zs(n_layers,3), as(n_layers,3), errors(n_layers,3))
+
+    do i = 1,n_layers
+        bias(i,:) = (/ 1, 2, 3 /)
+    end do
+
+    call random_number(as(1,:))
+    ! call random_number(bias)
     call random_number(weights)
-    call feedforward(a, weights, bias)
-    print*, a
+
+    do i = 1,n_layers
+        call feedforward(as(i,:), weights(i,:,:), bias(i,:), zs(i,:))
+    end do
+
+
+    example = (/ 40, 50, 60 /)
+    call output_error(example, as(n_layers,:), zs(n_layers,:), errors(1,:))
+
+    do i = 1,n_layers - 1
+        errors(i + 1,:) = errors(i,:)
+        call backpropagate(errors(i+1,:), weights(n_layers - i,:,:), zs(n_layers - i,:))
+    end do
+
+    do i = 1,n_layers
+        call update_weights(weights(i,:,:), errors(i,:), as(i-1,:), 0.01)
+        call update_bias(bias(i,:), errors(i,:), 0.01)
+    end do
 
 contains
 
-    subroutine feedforward(a, weights, bias)
-        real,dimension(:,:),intent(in) :: weights
-        real,dimension(:),intent(in) :: bias
-        real,dimension(:),intent(inout) :: a
+    ! activation of previous layer
+    subroutine update_weights(weights, error, activation, mu)
+        real,intent(inout) :: weights(:,:)
+        real,intent(in) :: error(:), activation(:), mu
+
+        weights = weights - mu * dot_product(error, activation)
+    end subroutine update_weights
+
+    subroutine update_bias(bias, error, mu)
+        real,intent(inout) :: bias(:)
+        real,intent(in) :: error(:), mu
+        
+        bias = bias - mu * sum(error)
+    end subroutine update_bias
+
+    subroutine feedforward(a, weights, bias, z)
+        real,intent(in) :: weights(:,:), bias(:)
+        real,dimension(:),intent(inout) :: a, z
         integer :: i
 
-        a = matmul(a, weights) + bias
-        a = (/ (sigmoid(a(i)), i=1, size(a)) /)
+        z = matmul(a, weights) + bias
+        a = (/ (sigmoid(z(i)), i=1, size(a)) /)
     end subroutine feedforward
 
-    pure function sigmoid(z) result (r)
+    ! computes the "error" in the output layer
+    ! given by d^L_j = \frac{\delta C}{\delta a^L_j} \sigma'(z^L_j)
+    subroutine output_error(example, output, zs, error)
+        real,dimension(:),intent(in) :: example, output, zs
+        real,dimension(:),intent(out) :: error
+
+        error = (/ (output(i) - example(i), i=1, size(error)) /)
+        error = (/ (error(i) * sigmoid1(zs(i)), i=1, size(error)) /)
+    end subroutine output_error
+
+    subroutine backpropagate(error, weights, zs)
+        real,dimension(:),intent(inout) :: error
+        real,intent(in) :: weights(:,:), zs(:)
+
+        error = matmul(transpose(weights), error)
+        error = (/ (error(i) * sigmoid1(zs(i)), i=1, size(error)) /)
+    end subroutine backpropagate
+
+    pure function sigmoid(z) result(r)
         real,intent(in) :: z
         real :: r
         
         r = (1 + exp(-z))**(-1)
     end function sigmoid
+
+    pure function sigmoid1(z) result(r)
+        real,intent(in) :: z
+        real :: r
+
+        r = sigmoid(-z) * (1 - sigmoid(-z))
+    end function sigmoid1
 
 end program nn
